c8b32ab72221bacf47d61c7a3397b7d1ee8bd9fd
agustinlongarini <84213966+agustinlongarini@users.noreply.github.com>
2025-05-25 20:52:39 -0300

    first commit

---
+++ src/main/java/com/grupo6/stockline/StocklineApplication.java
@@ -0,0 +1,13 @@
+package com.grupo6.stockline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocklineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocklineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo6/stockline/StocklineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo6.stockline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocklineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
