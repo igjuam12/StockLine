33ebd74782499aad5edb40abd69e4525c387e13d
agustinlongarini <84213966+agustinlongarini@users.noreply.github.com>
2025-06-03 14:39:42 -0300

    Funcionalidad baja generica

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -9,3 +9,18 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 });
+
+function abrirModal(elemento, event) {
+    event.preventDefault();
+    const id = elemento.getAttribute("data-id");
+    const path = elemento.getAttribute("data-path");
+    const form = document.getElementById("formBaja");
+    form.action = `/${path}/${id}/baja`;
+    console.log("Form action:", form.action);
+    document.getElementById("modalBaja").classList.remove("hidden");
+}
+
+function cerrarModal() {
+    document.getElementById("modalBaja").classList.add("hidden");
+}
+
